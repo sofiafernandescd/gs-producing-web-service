640fd169c8646fcee749b9945f77df297e0b09e8
sofiafernandescd <sofiafernandescd@gmail.com>
2019-09-18 17:27:26 +0100

    producing

---
+++ initial/src/main/java/hello/CountryEndpoint.java
@@ -0,0 +1,41 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.spring.guides.gs_producing_web_service.ExistingCountryRequest;
+import io.spring.guides.gs_producing_web_service.ExistingCountryResponse;
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+	
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "existingCountryRequest")
+	@ResponsePayload
+	public ExistingCountryResponse existingCountry(@RequestPayload ExistingCountryRequest request) {
+		ExistingCountryResponse response = new ExistingCountryResponse();
+		response.setCountry(countryRepository.existingCountry(request.getName()));
+		return response;
+	}
+}
